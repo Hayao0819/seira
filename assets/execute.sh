5db334b003c5de6ce24d023d891da629322186d6
hayao <hayao@fascode.net>
2024-09-19 22:29:29 +0900

    Add: execute.sh

---
+++ assets/execute.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -eu
+_TMP_DIR="$(mktemp -d)"
+echo "{{ .TarBallBase64 }}" | base64 -d >"$_TMP_DIR/tarball"
+mkdir -p "$_TMP_DIR/extract"
+tar -xf "$_TMP_DIR/tarball" -C "$_TMP_DIR/extract"
+echo "#!{{ .Shebang }}" >"$_TMP_DIR/execute"
+echo "source $_TMP_DIR/extract/{{ .Entrypoint }}" >>"$_TMP_DIR/execute"
+echo "{{ .Entrypoint }}" >>"$_TMP_DIR/execute"
+chmod +x "$_TMP_DIR/execute"
+"$_TMP_DIR/execute"
